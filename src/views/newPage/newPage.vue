ba279d351ba81d728ec52475a932c86936b03af9
GordonLz <451813289@qq.com>
2019-11-21 15:24:29 +0800

    初始化项目

---
+++ src/views/newPage/newPage.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    newPage  page
+  </div>
+</template>
+
+<script>
+export default {
+  name: '404'
+}
+</script>
+
+<style scoped>
+/* @import 'ant-design-vue/lib/style/themes/default.less'; */
+</style>
