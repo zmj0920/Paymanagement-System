198ba02bbb86a9bd1bed7ccd4ce8c2e7348385cf
zmj0920 <mj960920@163.com>
2019-11-22 23:06:10 +0800

    table

--- src/views/channel_management/fk_rules/Index.vue
+++ src/views/channel_management/fk_rules/Index.vue
@@ -1,18 +1,81 @@
 <template>
   <div>
-    风控规则
+    <div class="table-page-search-wrapper">
+      <a-form layout="inline">
+        <a-row :gutter="48">
+          <a-col :md="8" :sm="24">
+            <a-form-item label="ID名称">
+              <a-input placeholder="ID名称" />
+            </a-form-item>
+          </a-col>
+          <a-col :md="!advanced && 8 || 24" :sm="24">
+            <span
+              class="table-page-search-submitButtons"
+              :style="advanced && { float: 'right', overflow: 'hidden' } || {} "
+            >
+              <a-button type="primary" @click="$refs.table.refresh(true)">查询</a-button>
+              <a-button style="margin-left: 8px" @click="() => queryParam = {}">新增</a-button>
+            </span>
+          </a-col>
+        </a-row>
+      </a-form>
+    </div>
+    <a-table bordered :columns="columns4" :dataSource="dataSource" :pagination="true">
+      <template slot="operation" slot-scope="text, record">
+        <a-button size="small" type="primary" @click="handleDelete(record)" class="btn">编辑</a-button>
+        <a-button size="small" type="primary" @click="handleDelete(record)" class="btn">新增通道</a-button>
+        <a-button size="small" type="primary" @click="handleDelete(record)" class="btn">删除</a-button>
+      </template>
+      <template slot="interest" slot-scope="text, ">
+        <a-badge status="success" text="成功" v-if="text=='1'" />
+        <a-badge status="error" text="报错" v-else-if="text=='2'" />
+        <a-badge status="default" text="正常" v-else-if="text=='3'" />
+        <a-badge status="processing" text="进行中" v-else-if="text=='4'" />
+        <a-badge status="warning" text="警告" v-else />
+      </template>
+    </a-table>
   </div>
 </template>
-
 <script>
+import { Modal, message } from 'ant-design-vue'
+import dataSource from './data'
 export default {
+  name: 'HighTable',
+  components: {
+    'a-modal': Modal
+  },
   data () {
     return {
+      dataSource: dataSource.data,
+      columns4: dataSource.highColumns4,
+      sortedInfo: {
+        order: 'descend',
+        columnKey: 'age'
+      },
+      params: {
+        page: 1
+      }
     }
-  }
+  },
+  computed: {},
+  methods: {
+    handleDelete (record) {
+      Modal.confirm({
+        title: '确认',
+        content: '您确认要删除此条数据吗？',
+        onOk: () => {
+          message.success('删除成功')
+          this.request()
+        }
+      })
+    }
+  },
+  mounted () {}
 }
 </script>
 
-<style scoped>
-/* @import 'ant-design-vue/lib/style/themes/default.less'; */
+<style scoped lang="less">
+.btn {
+ margin-left: 5px;
+}
 </style>
--- src/views/channel_management/td_passageway/Index.vue
+++ src/views/channel_management/td_passageway/Index.vue
@@ -1,13 +1,144 @@
+/* eslint-disable vue/valid-template-root */
 <template>
-  <div>
-    通道
-  </div>
+  <a-card :bordered="false">
+    <div class="table-page-search-wrapper">
+      <a-form layout="inline">
+        <a-row :gutter="48">
+          <a-col :md="8" :sm="24">
+            <a-form-item label="ID名称">
+              <a-input placeholder="ID名称"/>
+            </a-form-item>
+          </a-col>
+          <a-col :md="8" :sm="24">
+            <a-form-item label="分组">
+              <a-select placeholder="请选择分组" default-value="0">
+                <a-select-option value="0">全部</a-select-option>
+                <a-select-option value="1">关闭</a-select-option>
+                <a-select-option value="2">运行中</a-select-option>
+              </a-select>
+            </a-form-item>
+          </a-col>
+          <a-col :md="8" :sm="24">
+            <a-form-item label="状态">
+              <a-select placeholder="请选择状态" default-value="0">
+                <a-select-option value="0">全部</a-select-option>
+                <a-select-option value="1">关闭</a-select-option>
+                <a-select-option value="2">运行中</a-select-option>
+              </a-select>
+            </a-form-item>
+          </a-col>
+          <a-col :md="8" :sm="24">
+            <a-form-item label="渠道类型">
+              <a-select placeholder="请选择渠道类型" default-value="0">
+                <a-select-option value="0">全部</a-select-option>
+                <a-select-option value="1">关闭</a-select-option>
+                <a-select-option value="2">运行中</a-select-option>
+              </a-select>
+            </a-form-item>
+          </a-col>
+          <a-col :md="8" :sm="24">
+            <a-form-item label="交易类型">
+              <a-select placeholder="请选择交易类型" default-value="0">
+                <a-select-option value="0">全部</a-select-option>
+                <a-select-option value="1">关闭</a-select-option>
+                <a-select-option value="2">运行中</a-select-option>
+              </a-select>
+            </a-form-item>
+          </a-col>
+
+          <a-col :md="!advanced && 8 || 24" :sm="24">
+            <span class="table-page-search-submitButtons" :style="advanced && { float: 'right', overflow: 'hidden' } || {} ">
+              <a-button type="primary" @click="$refs.table.refresh(true)">查询</a-button>
+              <a-button style="margin-left: 8px" @click="() => queryParam = {}">新增聚道</a-button>
+            </span>
+          </a-col>
+        </a-row>
+      </a-form>
+    </div>
+    <template>
+      <a-table :columns="columns" :dataSource="data">
+        <a slot="name" slot-scope="text" href="javascript:;">{{ text }}</a>
+        <span slot="customTitle"><a-icon type="smile-o" /> Name</span>
+        <span slot="tags" slot-scope="tags">
+          <a-tag
+            v-for="tag in tags"
+            :color="tag==='loser' ? 'volcano' : (tag.length > 5 ? 'geekblue' : 'green')"
+            :key="tag"
+          >
+            {{ tag.toUpperCase() }}
+          </a-tag>
+        </span>
+        <span slot="action" slot-scope="text, record">
+          <a href="javascript:;">Invite 一 {{ record.name }}</a>
+          <a-divider type="vertical" />
+          <a href="javascript:;">Delete</a>
+          <a-divider type="vertical" />
+          <a href="javascript:;" class="ant-dropdown-link"> More actions <a-icon type="down" /> </a>
+        </span>
+      </a-table>
+    </template>
+  </a-card>
 </template>
 
 <script>
+const columns = [
+  {
+    dataIndex: 'name',
+    key: 'name',
+    slots: { title: 'customTitle' },
+    scopedSlots: { customRender: 'name' }
+  },
+  {
+    title: 'Age',
+    dataIndex: 'age',
+    key: 'age'
+  },
+  {
+    title: 'Address',
+    dataIndex: 'address',
+    key: 'address'
+  },
+  {
+    title: 'Tags',
+    key: 'tags',
+    dataIndex: 'tags',
+    scopedSlots: { customRender: 'tags' }
+  },
+  {
+    title: 'Action',
+    key: 'action',
+    scopedSlots: { customRender: 'action' }
+  }
+]
+const data = [
+  {
+    key: '1',
+    name: 'John Brown',
+    age: 32,
+    address: 'New York No. 1 Lake Park',
+    tags: ['nice', 'developer']
+  },
+  {
+    key: '2',
+    name: 'Jim Green',
+    age: 42,
+    address: 'London No. 1 Lake Park',
+    tags: ['loser']
+  },
+  {
+    key: '3',
+    name: 'Joe Black',
+    age: 32,
+    address: 'Sidney No. 1 Lake Park',
+    tags: ['cool', 'teacher']
+  }
+]
+
 export default {
   data () {
     return {
+      data,
+      columns
     }
   }
 }
