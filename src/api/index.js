823371db70825ab65087f0b58962a6dbc8fe51e5
zmj0920 <mj960920@163.com>
2019-11-23 16:37:39 +0800

    update 渠道

--- src/api/index.js
+++ src/api/index.js
@@ -2,6 +2,7 @@ const ipUrl = 'http://47.56.180.125'
 
 const servicePath = {
   userLogin: ipUrl + '/auth/local', //  登录接口
-  userRegister: ipUrl + '/auth/local/register' // 注册用户
+  userRegister: ipUrl + '/auth/local/register', // 注册用户
+  channels: ipUrl + '/channels' // 渠道
 }
 export default servicePath
--- src/views/channel_management/qd_channel/Index.vue
+++ src/views/channel_management/qd_channel/Index.vue
@@ -1,71 +1,38 @@
 <template>
   <div>
-    <s-table
-      ref="table"
-      size="default"
-      :rowKey="(record) => record.data.id"
-      :columns="columns"
-      :data="loadData"
-      :rowSelection="{ selectedRowKeys: selectedRowKeys, onChange: onSelectChange }"
-    >
-    </s-table>
+    {{ token }}
   </div>
 </template>
 <script>
-import { STable } from '@/components'
-
+import axios from 'axios'
+import servicePath from '@/api'
 export default {
   components: {
-    STable
+
   },
   data () {
     return {
-      columns: [
-        {
-          title: '规则编号',
-          dataIndex: 'no'
-        },
-        {
-          title: '描述',
-          dataIndex: 'description'
-        },
-        {
-          title: '服务调用次数',
-          dataIndex: 'callNo',
-          sorter: true,
-          needTotal: true,
-          customRender: (text) => text + ' 次'
-        },
-        {
-          title: '状态',
-          dataIndex: 'status',
-          needTotal: true
-        },
-        {
-          title: '更新时间',
-          dataIndex: 'updatedAt',
-          sorter: true
-        }
-      ],
-      // 查询条件参数
-      queryParam: {},
-      // 加载数据方法 必须为 Promise 对象
-      loadData: parameter => {
-        return this.$http.get('/service', {
-          params: Object.assign(parameter, this.queryParam)
-        }).then(res => {
-          return res.result
-        })
-      },
-      selectedRowKeys: [],
-      selectedRows: []
     }
   },
   methods: {
-    onSelectChange (selectedRowKeys, selectedRows) {
-      this.selectedRowKeys = selectedRowKeys
-      this.selectedRows = selectedRows
+    init () {
+      axios({
+        url: servicePath.channels,
+        method: 'get',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${this.$store.getters.token}`
+        }
+      }).then(res => {
+        console.log(res)
+      }).catch(err => console.log(err))
+        .finally(() => {
+
+        })
     }
+  },
+  mounted () {
+    this.init()
   }
 }
 </script>
